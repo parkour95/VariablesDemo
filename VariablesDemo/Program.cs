bfba19816daaf00a0b53a5b2cfb34f87a2504c45
Parker Richmond <prjax95@gmail.com>
2021-10-22 12:25:40 -0400

    Add project files.

---
+++ VariablesDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VariablesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Ronaldo";
+
+            int dogAge = 3;
+
+            char initial = 'R';
+
+            bool knowsTricks = true;
+
+            double height = 2.5;
+
+            decimal weight = 35.6m;
+
+            Console.WriteLine($"My dog's name is {dogName}. He is {dogAge}. It's {knowsTricks} that he knows tricks. ");
+            Console.WriteLine($"He is {height} feet tall, and weighs {weight} pounds. We call {dogName}, {initial} for short.");
+        }
+    }
+}
